ea2b7e111c3acb802d3dd9dcc4c959aeae1fd357
Peter <Peter.Yang@monitorerp.com>
2024-04-03 10:39:45 +0800

    update

--- src/app/six.js
+++ src/app/six.js
@@ -4,79 +4,159 @@ export let WFD= [
         content:'The celebrated theory is still the source of great controversy.',
         description:'这一著名的理论至今仍是巨大争议的根源。'
     },
-    // {
-    //     index: 2,
-    //     content:'I thought a good architectural structure should be useful, durable and beautiful.',
-    //     description:'我以为一个好的建筑结构应该是有用的，耐用的，美观的。'
-    // },
-    // {
-    //     index: 3,
-    //     content:'A group meeting will be held tomorrow in the library conference room.',
-    //     description:'明天将在图书馆会议室举行小组会议。'
-    // },
-    // {
-    //     index: 4,
-    //     content:'A number of students have volunteer jobs.',
-    //     description:'许多学生从事志愿工作。'
-    // },
-    // {
-    //     index: 5,
-    //     content:'Educational level is found to be related to social and economic background.',
-    //     description:'教育水平与社会和经济背景有关。'
-    // },
-    // {
-    //     index: 6,
-    //     content:'We can all meet at my office after the lecture.',
-    //     description:'讲座结束后我们可以在我的办公室见面。'
-    // },
-    // {
-    //     index: 7,
-    //     content:'Tutorials are scheduled in the final week of the term.',
-    //     description:'辅导课安排在学期的最后一周。'
-    // },
-    // {
-    //     index: 8,
-    //     content:'You can make an appointment to meet the librarian.',
-    //     description:'你可以和图书管理员约个时间。'
-    // },
-    // {
-    //     index: 9,
-    //     content:'Affordable housing is an important issue for all members of society.',
-    //     description:'经济适用房对社会所有成员来说都是一个重要问题。'
-    // },
-    // {
-    //     index: 10,
-    //     content:'Agenda items should be submitted by the end of the day.',
-    //     description:'议程项目应在当天结束前提交。'
-    // },
-    // {
-    //     index: 11,
-    //     content:'All dissertations must be accompanied by a submission form.',
-    //     description:'所有论文必须附上提交表格。'
-    // },
-    // {
-    //     index: 12,
-    //     content:'All industries are a system of inputs, processes, outputs and feedback.',
-    //     description:'所有行业都是一个输入、处理、输出和反馈的系统。'
-    // },
-    // {
-    //     index: 13,
-    //     content:'All of the assignments must be submitted in person to the faculty office.',
-    //     description:'所有的作业必须亲自递交到学院办公室。'
-    // },
-    // {
-    //     index: 14,
-    //     content:'Economic development needs to be supported by the government.',
-    //     description:'经济发展需要政府的支持。'
-    // },
-    // {
-    //     index: 15,
-    //     content:'All of your assignments are due by tomorrow.',
-    //     description:'你明天应该交所有的作业。'
-    // },
-    // {
-    //     index: 16,
-    //     content:'All students are expected to attend ten lab sessions per semester.',
-    //     description:'所有学生每学期需参加十次实验。'
-    // },
+    {
+        index: 2,
+        content:'I thought a good architectural structure should be useful,durable and beautiful.',
+        description:'我以为一个好的建筑结构应该是有用的，耐用的，美观的。'
+    },
+    {
+        index: 3,
+        content:'A group meeting will be held tomorrow in the library conference room.',
+        description:'明天将在图书馆会议室举行小组会议。'
+    },
+    {
+        index: 4,
+        content:'A number of students have volunteer jobs.',
+        description:'许多学生从事志愿工作。'
+    },
+    {
+        index: 5,
+        content:'Educational level is found to be related to social and economic background.',
+        description:'教育水平与社会和经济背景有关。'
+    },
+    {
+        index: 6,
+        content:'We can all meet at my office after the lecture.',
+        description:'讲座结束后我们可以在我的办公室见面。'
+    },
+    {
+        index: 7,
+        content:'Tutorials are scheduled in the final week of the term.',
+        description:'辅导课安排在学期的最后一周。'
+    },
+    {
+        index: 8,
+        content:'You can make an appointment to meet the librarian.',
+        description:'你可以和图书管理员约个时间。'
+    },
+    {
+        index: 9,
+        content:'Affordable housing is an important issue for all members of society.',
+        description:'经济适用房对社会所有成员来说都是一个重要问题。'
+    },
+    {
+        index: 10,
+        content:'Agenda items should be submitted by the end of the day.',
+        description:'议程项目应在当天结束前提交。'
+    },
+    {
+        index: 11,
+        content:'All dissertations must be accompanied by a submission form.',
+        description:'所有论文必须附上提交表格。'
+    },
+    {
+        index: 12,
+        content:'All industries are a system of inputs,processes,outputs and feedback.',
+        description:'所有行业都是一个输入、处理、输出和反馈的系统。'
+    },
+    {
+        index: 13,
+        content:'All of the assignments must be submitted in person to the faculty office.',
+        description:'所有的作业必须亲自递交到学院办公室。'
+    },
+    {
+        index: 14,
+        content:'Economic development needs to be supported by the government.',
+        description:'经济发展需要政府的支持。'
+    },
+    {
+        index: 15,
+        content:'All of your assignments are due by tomorrow.',
+        description:'你明天应该交所有的作业。'
+    },
+    {
+        index: 16,
+        content:'All students are expected to attend ten lab sessions per semester.',
+        description:'所有学生每学期需参加十次实验。'
+    },
+    {
+        index: 17,
+        content:'Leaving valuable possessions unattended in public place is risky.',
+        description:'把贵重物品放在公共场所无人看管是很危险的。'
+    },
+    {
+        index: 18,
+        content:'The toughest part of postgraduate education is funding.',
+        description:'研究生教育最困难的部分是资金。'
+    },
+    {
+        index: 19,
+        content:'All writers, consciously or unconsciously,represent their own culture.',
+        description:'所有的作家，自觉或不自觉地，都代表着他们自己的文化。'
+    },
+    {
+        index: 20,
+        content:'Our view is that educational reforms have been inadequately implemented.',
+        description:'我们的看法是，教育改革执行得不够充分。'
+    },
+    {
+        index: 21,
+        content:'Most scientists believe that climate change threatens the lives on earth.',
+        description:'大多数科学家认为气候变化威胁地球上的生命。'
+    },
+    {
+        index: 22,
+        content:'Although sustainable development is not easy,it is an unavoidable responsibility.',
+        description:'可持续发展虽然不容易，但是一项不可推卸的责任。'
+    },
+    {
+        index: 23,
+        content:'Americans have typically defined the process of plant growth in quantitative terms.',
+        description:'美国人通常用定量术语来定义植物生长的过程。'
+    },
+    {
+        index: 24,
+        content:'An ancient text may hold secrets which were lost centuries ago.',
+        description:'古文可能藏有几个世纪前就消失了的秘密。'
+    },
+    {
+        index: 25,
+        content:'An introduction is an essential element of presentation.',
+        description:'简介是演讲的基本要素。'
+    },
+    {
+        index: 26,
+        content:'And in that regard, as well as in other regards,it stands as an important contribution.',
+        description:'在这方面以及在其他方面，它是一个重要的贡献。'
+    },
+    {
+        index: 27,
+        content:'Animals raised in captivity behave differently than their wild counterparts.',
+        description:'圈养的动物和野生的动物行为不同。'
+    },
+    {
+        index: 28,
+        content:'The assignments should be submitted to the department office before the deadlines.',
+        description:'作业须于截止日期前送交系办公室。'
+    },
+    {
+        index: 29,
+        content:'Attracting skilled workers to the city with expensive housing is hard.',
+        description:'吸引技术工人到这个住房昂贵的城市是困难的。'
+    },
+    {
+        index: 30,
+        content:'Avoid confusing the cause and effect of these changes.',
+        description:'避免混淆这些变化的原因和结果。'
+    },
+    {
+        index: 31,
+        content:'Before attending classes,you must register online or by post.',
+        description:'在参加课程之前，你必须在网上或邮寄注册。'
+    },
+    {
+        index: 32,
+        content:'Before completing the exercise, you need to read the remaining chapters.',
+        description:'在完成练习之前，你需要阅读剩下的章节。'
+    },
 ]
