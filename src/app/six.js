3063c9b35d46be27957d407beee810abb668a834
Peter <Peter.Yang@monitorerp.com>
2024-04-09 15:24:30 +0800

    update

--- src/app/six.js
+++ src/app/six.js
@@ -91,7 +91,7 @@ export let WFD= [
     },
     {
         index: 61,
-        content:'Human beings compete with other living things for resources and space. ',
+        content:'Human beings compete with other living things for resources and space.',
         description:'人类与其他生物竞争资源和空间。'
     },
     {
@@ -111,18 +111,18 @@ export let WFD= [
     },
     {
         index: 77,
-        content:'Important details from the argument are missing in the summary. ',
+        content:'Important details from the argument are missing in the summary.',
         description:'摘要中缺少了论证中的重要细节。'
     },
     {
         index: 86,
-        content:'It may not be possible to solve the problem easily. ',
+        content:'It may not be possible to solve the problem easily.',
         description:'想要轻易解决这个问题也许是不可能的。'
     },
     {
         index: 88,
         content:'Laws protect consumer rights and help to avoid market problems.',
-        description:'法律保护消费者权利，帮助避免市场问题。'
+        description:'法律保护消费者权利,帮助避免市场问题。'
     },
     {
         index: 92,
@@ -156,7 +156,487 @@ export let WFD= [
     },
     {
         index: 136,
-        content:'Remember, the prestigious election of stewardship has strict eligibility criteria.',
+        content:'Remember,the prestigious election of stewardship has strict eligibility criteria.',
         description:'记住，这场有声望的管理层选举有严格的资格标准。'
+    },
+    {
+        index: 137,
+        content:'Resources and materials are on hold at the library reference desk.',
+        description:'图书馆的咨询台保留着资源和资料。'
+    },
+    {
+        index: 160,
+        content:'Students requiring an extension should apply sooner rather than later.',
+        description:'要求延期的学生应尽早申请，不宜推迟。'
+    },
+    {
+        index: 175,
+        content:'The application processes may take longer than expected.',
+        description:'申请过程可能比预期的时间更长。'
+    },
+    {
+        index: 176,
+        content:'The archeologist_s new discoveries stand out in previously overlooked foundations.',
+        description:'这位考古学家的新发现在以前被忽视的基础上更引人注目。'
+    },
+    {
+        index: 186,
+        content:'The chemistry building is located near the entrance to the campus.',
+        description:'化学楼位于校园入口处附近。'
+    },
+    {
+        index: 187,
+        content:'The city_s founders created a set of rules that became law.',
+        description:'这座城市的创建者创建了一套规则，并最终成为法律。'
+    },
+    {
+        index: 191,
+        content:'The course will help students to improve their pronunciation skills.',
+        description:'这门课程将帮助学生提高他们的发音技巧。'
+    },
+    {
+        index: 194,
+        content:'The designers will complete the plan later today.',
+        description:'设计师们将在今天晚些时候完成这个设计图。'
+    },
+    {
+        index: 201,
+        content:'The essay will be published once the research is finished.',
+        description:'一旦研究完成，这篇文章就会发表。'
+    },
+    {
+        index: 213,
+        content:'The island is located at the south end of the bay.',
+        description:'这个岛位于海湾的南端。'
+    },
+    {
+        index: 214,
+        content:'The key difference between courses is the kind of assessment.',
+        description:'课程之间的主要区别在于评估方式的不同。'
+    },
+    {
+        index: 231,
+        content:'The railway makes long-distance travel possible for everyone.',
+        description:'铁路使每个人都能进行长途旅行。'
+    },
+    {
+        index: 244,
+        content:'The students were instructed to submit their assignments before Friday.',
+        description:'学生们被要求在星期五之前交作业。'
+    },
+    {
+        index: 284,
+        content:'Traffic is the main cause of air pollution in many cities.',
+        description:'在许多城市，交通是空气污染的主要原因。'
+    },
+    {
+        index: 287,
+        content:'Tribes vied with each other to build monolithic statues.',
+        description:'部落为了建造巨型雕像而互相竞争。'
+    },
+    {
+        index: 289,
+        content:'Understanding how to use the library will save your time.',
+        description:'了解如何使用图书馆会节省你的时间。'
+    },
+    {
+        index: 296,
+        content:'We can_t consider any increase in our price at this stage.',
+        description:'在这个阶段，我们不能考虑任何的涨价。'
+    },
+    {
+        index: 298,
+        content:'We study science to understand and appreciate the world around us.',
+        description:'我们学习科学是为了理解和欣赏我们周围的世界。'
+    },
+    {
+        index: 307,
+        content:'The essay is easy to write once the research is completed.',
+        description:'一旦研究完成，这篇文章很容易写了。'
+    },
+    {
+        index: 308,
+        content:'Years of training are required to become a medical specialist.',
+        description:'成为医学专家需要多年的训练。'
+    },
+    {
+        index: 356,
+        content:'They developed a unique approach to training their employees.',
+        description:'他们开发了一种独特的方法来培训他们的员工。'
+    },
+    {
+        index: 367,
+        content:'The university has invested in new technologies designed to support learning.',
+        description:'大学已投资于新技术，旨在支持学习。'
+    },
+    {
+        index: 370,
+        content:'A good academic paper should present a clear argument.',
+        description:'一篇好的学术论文应该呈现一个清晰的论点。'
+    },
+    {
+        index: 386,
+        content:'In this language course, we focus on both fluency and accuracy.',
+        description:'在这门语言课程中，我们注重流利度和准确度。'
+    },
+    {
+        index: 399,
+        content:'Students are encouraged to monitor their own attendance.',
+        description:'我们鼓励学生自己监督自己的出勤率。'
+    },
+    {
+        index: 405,
+        content:'Continuing students will be sent the necessary application forms.',
+        description:'继续就读的学生将收到所需的申请表格。'
+    },
+    {
+        index: 406,
+        content:'The students will meet their new teachers after the summer vacation.',
+        description:'暑假过后，学生们将会见到他们的新老师。'
+    },
+    {
+        index: 411,
+        content:'The department has a higher-than-normal proportion of postgraduate students.',
+        description:'这个学院的研究生比例高于正常水平。'
+    },
+    {
+        index: 415,
+        content:'The plight of local wildlife has been ignored by developers.',
+        description:'当地野生动物的困境一直被开发商忽视。'
+    },
+    {
+        index: 417,
+        content:'A professor took one year off to work on her book.',
+        description:'一位教授休了一年的假，专注于撰写她的书籍。'
+    },
+    {
+        index: 419,
+        content:'Please move us to the meeting room for the next hour.',
+        description:'在接下来的一个小时里，请带我们到会议室去。'
+    },
+    {
+        index: 441,
+        content:'Food has become a political issue in the world.',
+        description:'食品已经成为世界上的一个政治问题。'
+    },
+    {
+        index: 443,
+        content:'Speed is defined as how quickly an object or a person moves.',
+        description:'速度的定义是物体或人移动的速度。'
+    },
+    {
+        index: 444,
+        content:'Animal and plant cells have a number of structures in common.',
+        description:'动物和植物的细胞有许多共同的结构。'
+    },
+    {
+        index: 450,
+        content:'Being bilingual does not necessarily mean having the ability to analyze the languages.',
+        description:'会说两种语言并不一定意味着有分析语言的能力。'
+    },
+    {
+        index: 456,
+        content:'You must hand in your essays by midday on Friday.',
+        description:'作业必须在星期五中午前提交。'
+    },
+    {
+        index: 458,
+        content:'Sugar is a compound including carbon, oxygen, and hydrogen atoms.',
+        description:'糖是一种包括碳、氧和氢原子的化合物。'
+    },
+    {
+        index: 463,
+        content:'The shipwreck of this year ruined some artifacts which were interested by historians.',
+        description:'今年的海难毁坏了一些历史学家感兴趣的文物。'
+    },
+    {
+        index: 465,
+        content:'Renovation works are currently being undertaken throughout the whole building.',
+        description:'整幢大楼目前正在进行翻新工程。'
+    },
+    {
+        index: 466,
+        content:'The bus for London will leave ten minutes later than planned.',
+        description:'开往伦敦的公共汽车将比原计划晚开10分钟。'
+    },
+    {
+        index: 468,
+        content:'A series of lectures showcasing economic research have been recorded.',
+        description:'一系列展示经济研究的讲座被录了下来。'
+    },
+    {
+        index: 470,
+        content:'Food containing ample calories provides little or no nutritional value.',
+        description:'含有过多热量的食物几乎没有提供营养价值。'
+    },
+    {
+        index: 476,
+        content:'Some vocational courses at institutions are funded by private enterprises.',
+        description:'一些机构的职业课程是由私营企业资助的。'
+    },
+    {
+        index: 479,
+        content:'Time and distance are used to calculate speed.',
+        description:'时间和距离被用来计算速度。'
+    },
+    {
+        index: 495,
+        content:'Castles were designed to intimidate both local people and enemies.',
+        description:'建造城堡是为了震慑当地人和敌人。'
+    },
+    {
+        index: 497,
+        content:'Blue whales are the largest animals that have ever lived.',
+        description:'蓝鲸是有史以来最大的动物。'
+    },
+    {
+        index: 498,
+        content:'The opening hours of the library are reduced during summer.',
+        description:'图书馆在夏季的开放时间减少了。'
+    },
+    {
+        index: 499,
+        content:'A laptop computer has been found in the biology lab.',
+        description:'在生物实验室里发现了一台笔记本电脑。'
+    },
+    {
+        index: 500,
+        content:'His appointment to the Minister of Culture was seen as a demotion.',
+        description:'他被任命为文化部长被认为是降职。'
+    },
+    {
+        index: 503,
+        content:'Muscle cells bring parts of the body closer together.',
+        description:'肌肉细胞使身体的各个部分紧密相连。'
+    },
+    {
+        index: 508,
+        content:'Photography can be very useful to geographical research.',
+        description:'摄影对地理研究很有用。'
+    },
+    {
+        index: 509,
+        content:'Request for late applications will not be accepted under any circumstances.',
+        description:'在任何情况下，都不接受逾期递交的申请。'
+    },
+    {
+        index: 513,
+        content:'Salt is produced from seawater or extracted from the ground.',
+        description:'盐是从海水中产生的或从地下提取的。'
+    },
+    {
+        index: 516,
+        content:'The news will not be received until the following week.',
+        description:'消息要到下一周才会收到。'
+    },
+    {
+        index: 517,
+        content:'You do not need specialist knowledge to enjoy this book.',
+        description:'你不需要专业知识就能欣赏这本书。'
+    },
+    {
+        index: 519,
+        content:'She has been in the library for a long time.',
+        description:'她在图书馆呆了很长时间了。'
+    },
+    {
+        index: 523,
+        content:'More graduate training is often needed after university study has finished.',
+        description:'大学学习结束后，需要更多的研究生培训。'
+    },
+    {
+        index: 526,
+        content:'The area has a variety of underwater habitats and species.',
+        description:'该地区有各种各样的水下栖息地和物种。'
+    },
+    {
+        index: 528,
+        content:'He was regarded as the foremost economist at his time.',
+        description:'他被认为是他那个时代最重要的经济学家。'
+    },
+    {
+        index: 530,
+        content:'Your ideas are discussed and debated in seminars and tutorials.',
+        description:'你的想法在研讨会和辅导课中被讨论和辩论。'
+    },
+    {
+        index: 532,
+        content:'Our study program equips students with essential skills for university.',
+        description:'我们的学习计划使学生具备上大学所需的基本技能。'
+    },
+    {
+        index: 534,
+        content:'Academic libraries across the world are steadily incorporating social media.',
+        description:'世界各地的学术图书馆都在逐步地使用社交媒体。'
+    },
+    {
+        index: 540,
+        content:'New developments in manufacturing are constantly changing the way we live.',
+        description:'制造业的新发展不断地改变着我们的生活方式。'
+    },
+    {
+        index: 542,
+        content:'Classical mechanics is sometimes considered as a branch of applied mathematics.',
+        description:'经典力学有时候被认为是应用数学的一个分支。'
+    },
+    {
+        index: 544,
+        content:'Students should leave their bags on the table by the door.',
+        description:'学生们应该把书包放在门边的桌子上。'
+    },
+    {
+        index: 545,
+        content:'This advanced course requires a basic knowledge of economic theory.',
+        description:'这门高阶课程要求具备基本的经济学理论知识。'
+    },
+    {
+        index: 547,
+        content:'New materials and techniques are changing the style of modern architecture.',
+        description:'新材料和新技术正在改变现代建筑的风格。'
+    },
+    {
+        index: 553,
+        content:'There will be a chemistry test in the class next week.',
+        description:'下周，班里将进行化学测试。'
+    },
+    {
+        index: 556,
+        content:'Sea levels are expected to rise during the next century.',
+        description:'海平面预计在下个世纪会上升。'
+    },
+    {
+        index: 575,
+        content:'Honey can be used as food and a health product.',
+        description:'蜂蜜既可以作为食品，也可以作为保健品。'
+    },
+    {
+        index: 577,
+        content:'Farmers need to adapt to the changes in climate.',
+        description:'农民需要适应气候的变化。'
+    },
+    {
+        index: 580,
+        content:'An effective business manager is always open to new ideas.',
+        description:'一个有能力的业务经理总是会接受新想法。'
+    },
+    {
+        index: 581,
+        content:'The department works closely with business communities.',
+        description:'该部门与商界紧密合作。'
+    },
+    {
+        index: 584,
+        content:'There is an accounting assignment for finance students.',
+        description:'金融专业的学生有一个会计作业。'
+    },
+    {
+        index: 585,
+        content:'Relying on natural ability will not get you far in science.',
+        description:'在科学/理科的学习上，依靠天生的能力不会使你走得很远。'
+    },
+    {
+        index: 588,
+        content:'We should never underestimate the power of creative design.',
+        description:'我们永远不能低估创意设计的力量。'
+    },
+    {
+        index: 589,
+        content:'Linguistics is the scientific study and analysis of language.',
+        description:'语言学是针对语言的科学研究和科学分析。'
+    },
+    {
+        index: 594,
+        content:'The orchestra will be led by a visiting conductor.',
+        description:'管弦乐队将由来访的指挥家来指挥。'
+    },
+    {
+        index: 596,
+        content:'Please return the reference book to the correct position on the library shelf.',
+        description:'请把参考书放回图书馆书架上正确的位置。'
+    },
+    {
+        index: 603,
+        content:'Marine environments have been destroyed by pollution and unsustainable development.',
+        description:'海洋环境已经被污染和不可持续的发展破坏了。'
+    },
+    {
+        index: 604,
+        content:'Designers need to keep up with the social trends.',
+        description:'设计师需要跟上社会潮流。'
+    },
+    {
+        index: 606,
+        content:'Extracurricular activities can help students develop more talents.',
+        description:'课外活动可以帮助学生发展更多的才能。'
+    },
+    {
+        index: 607,
+        content:'A new collection of articles has just been published.',
+        description:'一本新的文集刚刚出版。'
+    },
+    {
+        index: 609,
+        content:'A visit to the designed museum is of great value.',
+        description:'参观精心设计的博物馆是非常有价值的。'
+    },
+    {
+        index: 611,
+        content:'The digital camera has some advantages over traditional film.',
+        description:'数码相机比传统胶卷有一些优势。'
+    },
+    {
+        index: 614,
+        content:'Auditions for the university choir will be held next week.',
+        description:'大学合唱团的海选将于下周举行。'
+    },
+    {
+        index: 617,
+        content:'There is no fixed career path for a qualified journalist.',
+        description:'合格的记者没有固定的职业道路。'
+    },
+    {
+        index: 622,
+        content:'Trees benefit the city by soaking up the rainwater that runs off the roads.',
+        description:'树木通过吸收从道路上流下的雨水的方式来助益城市。'
+    },
+    {
+        index: 625,
+        content:'The commissioner will apportion the funds among all the authorities.',
+        description:'专员将把这笔资金分配给所有当局。'
+    },
+    {
+        index: 645,
+        content:'Teaching assistants play a vital role in the learning process.',
+        description:'教学助理在学习过程中起着至关重要的作用。'
+    },
+    {
+        index: 652,
+        content:'Rivers provide habitat and migration pathways for numerous aquatic species.',
+        description:'河流为许多水生物种提供了栖息地和迁徙途径。'
+    },
+    {
+        index: 653,
+        content:'The finding shows that chocolate can improve memory, immunity and mood.',
+        description:'研究结果表明，巧克力可以提高记忆力、免疫力，并改善情绪。'
+    },
+    {
+        index: 687,
+        content:'The extent of advertising to children is very much open to debate.',
+        description:'广告对儿童的影响程度是很容易引起争论的。'
+    },
+    {
+        index: 688,
+        content:'Rising sea temperatures are a sign of climate change.',
+        description:'海水温度上升是气候变化的征兆。'
+    },
+    {
+        index: 695,
+        content:'The key witnesses to the event have conflicting recollections.',
+        description:'该事件的主要目击者的回忆相互矛盾。'
+    },
+    {
+        index: 697,
+        content:'Students may only park their cars in authorized university parking spaces.',
+        description:'学生只能将车停在学校指定的停车位。'
     },
 ]
